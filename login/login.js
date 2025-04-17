6f282a470ad37819f5a01748f789331720598703
Buricko07 <adelmoburic1@gmail.com>
2025-04-17 20:49:40 +0200

    Add initial HTML structure and CSS for Quiz BiH application

--- login/login.js
+++ login/login.js
@@ -7,3 +7,30 @@ document.getElementById("togglePassword").addEventListener("click", function () 
     }
 });
 
+const Button = document.getElementById("button");
+
+async function login() {
+  const Email = document.getElementById("email").value;
+  const Password = document.getElementById("password").value;
+    try {
+      const response = await fetch(
+        "https://quiz-be-zeta.vercel.app/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: Email,
+            password: Password,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+}
+
+Button.addEventListener("click", login);
--- register/register.js
+++ register/register.js
@@ -16,3 +16,38 @@ document.getElementById("toggle-password").addEventListener("click", function ()
     }
 });
 
+const loginButton = document.getElementById("button");
+
+async function register() {
+  const Email = document.getElementById("email").value;
+  const Username = document.getElementById("username").value;
+  const Password = document.getElementById("password").value;
+  const ConfirmPassword = document.getElementById("repeat-password").value;
+  if (Password !== ConfirmPassword) {
+    print.innerHTML = "Passwords do not match!";
+  } else {
+    try {
+      const response = await fetch(
+        "https://quiz-be-zeta.vercel.app/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: Email,
+            username: Username,
+            password: Password,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+}
+
+loginButton.addEventListener("click", register);
+
