317f5ca288234f93b3941fd19f5e8b5e5b13b1bb
Buricko07 <adelmoburic1@gmail.com>
2025-04-17 20:55:42 +0200

    Add login and leaderboard features with associated styling

--- login/login.js
+++ login/login.js
@@ -7,30 +7,33 @@ document.getElementById("togglePassword").addEventListener("click", function () 
     }
 });
 
-const Button = document.getElementById("button");
+const loginButton = document.getElementById("login-btn");
 
 async function login() {
-  const Email = document.getElementById("email").value;
-  const Password = document.getElementById("password").value;
+    const Email = document.getElementById("email").value;
+    const Password = document.getElementById("password").value;
     try {
-      const response = await fetch(
-        "https://quiz-be-zeta.vercel.app/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: Email,
-            password: Password,
-          }),
-        }
-      );
-      const data = await response.json();
-      console.log(data);
+        const response = await fetch(
+            "https://quiz-be-zeta.vercel.app/auth/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: Email,
+                    password: Password,
+                }),
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+
+        localStorage.setItem("token", data.token);
+
     } catch (error) {
-      console.error("Error:", error);
+        console.error("Error:", error);
     }
 }
 
-Button.addEventListener("click", login);
+loginButton.addEventListener("click", login);
